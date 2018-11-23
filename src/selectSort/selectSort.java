1ec125b81ae94bc66ee3b1ff8935e6eca93dda8b
Jenya Bykov <mothersasg@yandex.ru>
2018-11-24 01:09:43 +0300

    +select sort
    +select sort

---
+++ src/selectSort/selectSort.java
@@ -0,0 +1,26 @@
+package selectSort;
+
+public class selectSort {
+    static int[] num = {1, -6, 23, -25, 44, 123, 1290, -456, 0};
+
+    public static void main(String[] args) {
+        selectionSort();
+        for (int n : num) {
+            System.out.print(n + " ");
+        }
+    }
+
+    public static void selectionSort() {
+        int min = 0, out = 0, in = 0;
+        for (int i = 0; i < num.length - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < num.length; j++) {
+                if (num[j] < num[min])
+                    min = j;
+            }
+            int k = num[i];
+            num[i] = num[min];
+            num[min] = k;
+        }
+    }
+}
