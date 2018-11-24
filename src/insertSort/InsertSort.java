0fba57931717c2abed93a0cbd0d8383b64f24cbc
Jenya Bykov <mothersasg@yandex.ru>
2018-11-24 15:55:08 +0300

    Create InsertSort.java
    InsertSort

---
+++ src/insertSort/InsertSort.java
@@ -0,0 +1,25 @@
+package insertSort;
+
+public class InsertSort {
+    static int[] num = {1, -6, 23, -25, 44, 123, 1290, -456, 0};
+
+    public static void main(String[] args) {
+        insertS();
+        for (int n : num) {
+            System.out.print(n + " ");
+        }
+    }
+
+    public static void insertS() {
+        int temp = 0, j = 0;
+        for (int i = 1; i < num.length ; i++) {
+            temp = num[i];
+            j = i;
+            while (j > 0 && num[j - 1] > temp) {
+                num[j] = num[j - 1];
+                j--;
+            }
+            num[j] = temp;
+        }
+    }
+}
