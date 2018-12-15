9d35709d63610a18ab845cb1e4536ea9c8c7957e
Jenya Bykov <mothersasg@yandex.ru>
2018-12-15 17:11:19 +0300

    QuickSort

---
+++ src/sort/Partition.java
@@ -0,0 +1,55 @@
+package sort;
+
+/**
+ * Разбиение массива на 2 части
+ */
+public class Partition {
+    int[] mas;
+
+    public static void main(String[] args) {
+        Partition part = new Partition();
+        part.start();
+    }
+
+    public void start() {
+        int n = 10;
+        mas = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            mas[i] = (int) (Math.random() * 100);
+            System.out.print(mas[i] + " ");
+        }
+        System.out.println();
+
+        int pivot = 50;                                 //Опорное значение
+
+        int posBetween = partirion(0, mas.length - 1, pivot);
+        System.out.println("Опорный элемент = " + pivot + " , разбиение между индексом " + posBetween);
+        for (int i = 0; i < n; i++) {
+            System.out.print(mas[i] + " ");
+        }
+    }
+
+    public int partirion(int left, int right, int pivot) {
+        int leftPtr = left - 1;
+        int rightPtr = right + 1;
+
+        while (true) {
+            while (leftPtr < right && mas[++leftPtr] < pivot)   //Поиск большего элемента
+                ;
+            while (rightPtr > left && mas[--rightPtr] > pivot)  //Поиск меньшего элемента
+                ;
+            if (leftPtr >= rightPtr)                            //Если указатели пересеклись
+                break;                                          //Разбиение закончено
+            else                                                //Иначе, меняем элементы местами
+                swap(leftPtr, rightPtr);
+        }
+        return leftPtr;
+    }
+
+    public void swap(int i, int j) {
+        int temp = mas[i];
+        mas[i] = mas[j];
+        mas[j] = temp;
+    }
+}
---
+++ src/sort/RecQuickSort.java
@@ -0,0 +1,63 @@
+package sort;
+
+public class RecQuickSort {
+    int[] mas;
+
+    public static void main(String[] args) {
+        RecQuickSort sort = new RecQuickSort();
+        sort.start();
+    }
+
+    public void start() {
+        int n = 10000;
+        mas = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            mas[i] = (int) (Math.random() * 100);
+            System.out.print(mas[i] + " ");
+        }
+        System.out.println();
+        //long t1 = System.currentTimeMillis();
+        quickSort(0, mas.length - 1);
+//        long t2 = System.currentTimeMillis();
+//        System.out.println("Time =  " + (t2 - t1) + " ms.");
+        for (int i = 0; i < n; i++) {
+            System.out.print(mas[i] + " ");
+        }
+    }
+
+    public void quickSort(int left, int right) {
+        if (right - left <= 0)
+            return ;
+        int pivot = mas[right];                                  //Крайний правый элемент, как опорный
+
+        int partition = partirion(left, right, pivot);           //Разбиение диапазона
+        quickSort(left, partition - 1);                    //Сортировка левой части
+        quickSort(partition + 1, right);                    //Сортировка правой части
+
+    }
+
+    public int partirion(int left, int right, int pivot) {
+        int leftPtr = left - 1;                                 //Левая граница(после++)
+        int rightPtr = right;                                   //Правая граница - 1(после --)
+
+        while (true) {
+            while (mas[++leftPtr] < pivot)                      //Поиск большего элемента
+                ;
+            while (rightPtr > 0 && mas[--rightPtr] > pivot)     //Поиск меньшего элемента
+                ;
+            if (leftPtr >= rightPtr)                            //Если указатели пересеклись
+                break;                                          //Разбиение закончено
+            else                                                //Иначе, меняем элементы местами
+                swap(leftPtr, rightPtr);
+        }
+        swap(leftPtr, right);                                   //Перестановка опорного элемента
+        return leftPtr;                                         //Возврат позиции опорного элемента
+    }
+
+    public void swap(int i, int j) {
+        int temp = mas[i];
+        mas[i] = mas[j];
+        mas[j] = temp;
+    }
+}
